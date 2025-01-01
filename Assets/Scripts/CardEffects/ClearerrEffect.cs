687929d386b55ebafa5d8236e3c30cb871b4626f
Pasu4 <pascalwieckhorst02@gmail.com>
2025-01-01 14:46:50 +0100

    Add new card effects

--- Assets/Scripts/CardEffects/ClearerrEffect.cs
+++ Assets/Scripts/CardEffects/ClearerrEffect.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using URandom = UnityEngine.Random;
 
 namespace Assets.Scripts.CardEffects
 {
     public class ClearerrEffect : CardEffect
     {
-        public override string Description => $"{nameStyleOpen}clearerr:{nameStyleClose} Erase all trojan cards in the deck.";
+        public int count;
+        public override string Description => $"{nameStyleOpen}clearerr({count}):{nameStyleClose} Erase up to {count} trojan cards from the deck.";
+
+        public ClearerrEffect(int count)
+        {
+            this.count = count;
+        }
 
         public override void OnPlay(BattleContext ctx)
         {
-            var cs = ctx.battleUI.deck.Where(c => c.card.type == Card.CardType.Trojan);
-            foreach(var c in cs)
+            BattleCardUI[] cs = ctx.battleUI.deck
+                .Where(c => c.card.type == Card.CardType.Trojan)
+                .OrderBy(_ => URandom.value)
+                .Take(count)
+                .ToArray();
+            foreach(BattleCardUI c in cs)
             {
                 ctx.battleUI.Erase(c);
             }
---
+++ Assets/Scripts/CardEffects/RandEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using UnityEngine;
+
+namespace Assets.Scripts.CardEffects
+{
+    public class RandEffect : CardEffect
+    {
+        long fileSize;
+
+        public override string Description => $"{nameStyleOpen}rand:{nameStyleClose} Add a random {Utils.FileSizeString(fileSize)} card to the hand.";
+
+        public RandEffect(long fileSize)
+        {
+            this.fileSize=fileSize;
+        }
+
+        public override void OnPlay(BattleContext ctx)
+        {
+            Card card = new Card(
+                Random.value.ToString("F7", CultureInfo.InvariantCulture)[2..] + ".log",
+                fileSize,
+                GameManager.Instance.GetFileSprite(".log")
+            );
+            ctx.battleUI.CreateHandCard(card);
+        }
+    }
+}
